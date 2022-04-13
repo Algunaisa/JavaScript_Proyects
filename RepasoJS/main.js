05d002aa2e97cd1df43334534d23970cdfefca35
Isa Flores <belen.isabel@hotmail.com>
2022-04-13 09:21:04 -0500

    Se aprendio el uso de variables y como mostrarlas mediante un div en html. El comentario muestra como mostrarlo directamente desde JC. Se cambio <scrip> al final para que se ejecute despues de la llamada al div datos

--- RepasoJS/main.js
+++ RepasoJS/main.js
@@ -1,1 +1,10 @@
-alert('Hola Mundo!!');
+//alert('Hola Mundo!!');
+
+var nombre = "Isa Flores";
+var altura = 185;
+
+var concatenacion = nombre + " " + altura;
+//document.write(concatenacion);
+var datos = document.getElementById("datos");
+
+datos.innerHTML = concatenacion;
