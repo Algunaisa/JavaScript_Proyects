c0548a5a3514098abebe1375b2e835a22513aeea
Isa Flores <belen.isabel@hotmail.com>
2022-04-13 11:30:14 -0500

    Modifique comentarios. Se agregaron estructuras de control if, else y for. Se creo funcion MuestraMiNombre con lo visto para mostrar el nombre añadiendole parametros y haciendolo flexible a cambios.

--- RepasoJS/main.js
+++ RepasoJS/main.js
@@ -1,26 +1,46 @@
 //alert('Hola Mundo!!');
 
+//Declarar variables
 var nombre = "Isa Flores";
-var altura = 185;
+var altura = 189;
 
-var concatenacion = nombre + " " + altura;
-//document.write(concatenacion);
-var datos = document.getElementById("datos");
+//Funcion
+MuestraMiNombre("Belen Perez",190);
 
-//Se muestra desde aqui en JC.
-//datos.innerHTML = concatenacion;
+//Estructuras de control
+if(altura >= 190){
+    datos.innerHTML += '<h1>Eres una persona alta</h1>';
+}else{
+    datos.innerHTML += '<h1>No eres una persona alta</h1>';
+}
 
-//Usando un div
-/*datos.innerHTML =   '<h1>Soy la caja de datos</h1>'
-                    +'<h2>Mi nombre es: '+nombre+'</h2>'
-                    +'<h3>Mido: '+altura+'</h3>';*/
+for(var i = 2000; i<=2020; i++){
+    //bloque de instrucciones
+    datos.innerHTML += "<h2>Estamos en el año: "+i;
+}
 
-//Template String
-datos.innerHTML = `
-    <h1>Soy la caja de datos</h1>
-    <h2>Mi nombre es: ${nombre}</h2>
-    <h3>Mido: ${altura}</h3>
-    `;
+function MuestraMiNombre(nombre, altura){
+  /*var concatenacion = nombre + " " + altura;
+
+    //Imprimir variables desde aqui en JC.
+    document.write(concatenacion);*/
+
+    //Usando un div y mostrar sin formato html
+    var datos = document.getElementById("datos");
+    //datos.innerHTML = concatenacion;
+
+    //Usando html
+    /*datos.innerHTML =   '<h1>Soy la caja de datos</h1>'
+                        +'<h2>Mi nombre es: '+nombre+'</h2>'
+                        +'<h3>Mido: '+altura+'</h3>';*/
+
+    //Usando Template String (comillas invertidas 96)
+    datos.innerHTML = `
+        <h1>Soy la caja de datos</h1>
+        <h2>Mi nombre es: ${nombre}</h2>
+        <h3>Mido: ${altura}</h3>
+        `;
+}
 
 
     
