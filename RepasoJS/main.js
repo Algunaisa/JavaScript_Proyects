d835587e0d0eb72bae5a10fa1c25a583d5a55036
Isa Flores <belen.isabel@hotmail.com>
2022-04-13 09:34:31 -0500

    La informacion del Div datos ahora contiene un Tamplate String, que usa comillas invertidas para enviar directamente informacion html al div seleccionado

--- RepasoJS/main.js
+++ RepasoJS/main.js
@@ -7,4 +7,10 @@ var concatenacion = nombre + " " + altura;
 //document.write(concatenacion);
 var datos = document.getElementById("datos");
 
-datos.innerHTML = concatenacion;
+//Template String
+datos.innerHTML = `
+    <h1>Soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2>
+    <h3>Mido: ${altura}</h3>
+    `;
+
