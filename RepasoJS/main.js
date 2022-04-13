778e59ecf233553a6608437a7a5e28c4ec69f15d
Isa Flores <belen.isabel@hotmail.com>
2022-04-13 11:43:55 -0500

    Se separaron las funciones para imprimir y para mostrar. La de mostrar retorna la variable con el formato de los datos que le pasen para mostrar en html; imprimir() asigna los valores y los muestra. Se comentaron las estructuras de control, no son necesarias por el momento.

--- RepasoJS/main.js
+++ RepasoJS/main.js
@@ -1,13 +1,6 @@
 //alert('Hola Mundo!!');
 
-//Declarar variables
-var nombre = "Isa Flores";
-var altura = 189;
-
-//Funcion
-MuestraMiNombre("Belen Perez",190);
-
-//Estructuras de control
+/*//Estructuras de control
 if(altura >= 190){
     datos.innerHTML += '<h1>Eres una persona alta</h1>';
 }else{
@@ -17,10 +10,32 @@ if(altura >= 190){
 for(var i = 2000; i<=2020; i++){
     //bloque de instrucciones
     datos.innerHTML += "<h2>Estamos en el año: "+i;
+}*/
+
+//Funciones con parametros y con paso de parametros
+function MuestraMiNombre(nombre, altura){
+  
+    //Usando html
+    /*var misDatos =   '<h1>Soy la caja de datos</h1>'
+                        +'<h2>Mi nombre es: '+nombre+'</h2>'
+                        +'<h3>Mido: '+altura+'</h3>';*/
+
+    //Usando Template String (comillas invertidas 96)
+    var misDatos = `
+        <h1>Soy la caja de datos</h1>
+        <h2>Mi nombre es: ${nombre}</h2>
+        <h3>Mido: ${altura}</h3>
+        `;
+
+        return misDatos;
 }
 
-function MuestraMiNombre(nombre, altura){
-  /*var concatenacion = nombre + " " + altura;
+//Funcion con los datos a imprimir
+function imprimir(){
+    //Declarar variables
+    /*var nombre = "Isa Flores";
+    var altura = 189;
+    var concatenacion = nombre + " " + altura;
 
     //Imprimir variables desde aqui en JC.
     document.write(concatenacion);*/
@@ -29,18 +44,9 @@ function MuestraMiNombre(nombre, altura){
     var datos = document.getElementById("datos");
     //datos.innerHTML = concatenacion;
 
-    //Usando html
-    /*datos.innerHTML =   '<h1>Soy la caja de datos</h1>'
-                        +'<h2>Mi nombre es: '+nombre+'</h2>'
-                        +'<h3>Mido: '+altura+'</h3>';*/
-
-    //Usando Template String (comillas invertidas 96)
-    datos.innerHTML = `
-        <h1>Soy la caja de datos</h1>
-        <h2>Mi nombre es: ${nombre}</h2>
-        <h3>Mido: ${altura}</h3>
-        `;
+    datos.innerHTML = MuestraMiNombre("Belen Perez",180);
 }
 
-
+//Llamar a la funcion
+imprimir();
     
